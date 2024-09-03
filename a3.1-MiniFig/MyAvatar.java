0720942fc9c05ebaf8a2add2a3f5a793565b0d56
ShaelynnNixonBSU <SHAELYNNNIXON@u.boisestate.edu>
2024-09-03 16:01:15 -0600

    Todos knocked off

--- a3.1-MiniFig/MyAvatar.java
+++ a3.1-MiniFig/MyAvatar.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -47,42 +48,53 @@ public class MyAvatar extends JPanel
 
 		// TODO: 1. Instantiate a new Point object called "anchor". Use "mid" as your x value and
 		//       "top" as your y value.
-		
+		Point anchor = new Point(mid,top);	
+
+
 		// TODO: 2. Instantiate a new MiniFig object and give the reference variable a name of a person, 
 		//       such as "bob". Use the MiniFig constructor with the following
 		//       parameters: MiniFig(g, scaleFactor, anchor)
 		
-		// TODO: 3. Create a new custom Color object. An example is shown below.
+		MiniFig shae = new MiniFig(g,scaleFactor, anchor);
 		
+		// TODO: 3. Create a new custom Color object. An example is shown below.
+		Color torsoColor = new Color (127,31,31);
+
 		// TODO: 4. Invoke the setTorsoColor(Color color) method on your MiniFig instance.
 		//       Use your color object as a parameter to change the shirt color.
 		//       This lets you change the color of "bob's" shirt. :)
+		shae.setTorsoColor(torsoColor);
 		
 		// TODO: 5. Invoke the draw() method on your MiniFig instance. This is where "bob" is displayed on the screen.
-		
+		//shae.draw();
 		// TODO: 6. Adjust the size of your Avatar's window. Notice how the avatar does not stay grounded
 		//       on the grass. To fix this, use the getBaseMidPoint() method to find the the base mid point of your
 		//       MiniFig. This method returns a Point object that represents the x,y coordinates at the
 		//       base of the MiniFig, right between its feet. 
 		//       Replace the hard-coded value of grassYOffset with the y value 
 		//       of the returned point.
-		int grassYOffset = 400;
-		
+		//int grassYOffset = 400;
+		Point basePoint = shae.getBaseMidPoint();
+		int grassYOffset = basePoint.y;
 		Color grassGreen = new Color (60,80,38);
 		g.setColor(grassGreen);
 		g.fillRect(0, grassYOffset, currentWidth, currentHeight - grassYOffset);
+
 
 		// TODO: 7. Create an Alias of for your MiniFig object and change the torso color of the alias.
 		//       If in step 2 you used the variable name "bob", you can create an alias named "robert"
 		//       using the following:
 		//       MiniFig robert = bob;
 		//       robert.setTorsoColor(Color.RED);
-
+		MiniFig robert = shae;
+		robert.setTorsoColor(Color.RED);
+		robert.draw();
 		
 	        // TODO: 8. Comment out the draw statement under TODO item 5 and then draw the original MiniFig 
 		//       below. If you used the variable name "bob" is would simply be the following:
 		//       bob.draw();
 		//       What color is Bob's Shirt?  Why?
+		//Red because Robert is an alias for Bob (shae in my case) not a duplicate, its two names for the same address. Two references to the same thing.
 	}
 
 
